db8f5e40620e693130e4872ea9ac28f3c86dd809
daizhenbest <daizhenbest@gmail.com>
2016-04-15 14:24:27 +0800

    Update training..

--- trainning_CNN.py
+++ trainning_CNN.py
@@ -1,16 +1,37 @@
 import os
 import sys
 import urllib
-import tensorflow.python.platform
+#import tensorflow.python.platform
 import numpy
-import tensorflow as tf
+#import tensorflow as tf
+import csv
 
 def test():
-    os.path.join()
-
-def LoadData():
-    os.path.join()
+    #os.path.join()
 	pass
+
+def LoadCategoryData():
+	image_list = list()
+	label_list = list()
+	fullFileName = os.path.join('data','all_category_data.csv')
+	csvfile = file(fullFileName, 'rb')
+	reader = csv.reader(csvfile)
+	index = 0
+	
+	data_list = list()
+	for line in reader:
+		if index != 0 :
+			data_list.append(line);
+			print line[0]
+		index +=1
+	csvfile.close()
+	
+	label_list=data_list[,1]
+	for dataItem in data_list:
+		image = Image.open(dataItem[0])   # image is a PIL image 
+		array = numpy.array(image)        # array is a numpy array
+		image_list.append(array);
+	return image_list,label_list
 	
 def Train():
 	pass
