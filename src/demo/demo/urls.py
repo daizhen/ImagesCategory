12fc780f5a5da2d7db70bd1934cbea3c9900f0b2
Dai Zhen <daizhenbest@gmail.com>
2016-05-17 15:10:09 +0930

    Add demo project

---
+++ src/demo/demo/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'demo.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'demo.views.default.default'),
+    (r'^site_media/(?P<path>.*)', 'django.views.static.serve',{'document_root': '/home/daizhen/projects/ImagesCategory/data/gray_images'}),
+)
