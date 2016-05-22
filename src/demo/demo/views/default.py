924b357b6b412ebd38e5afb1fad78e6a5255592a
Dai Zhen <daizhenbest@gmail.com>
2016-05-22 23:32:02 +0800

    Update the demo code

--- src/demo/demo/views/default.py
+++ src/demo/demo/views/default.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+
+import sys
+sys.path.append('/home/daizhen/projects/ImagesCategory/src/util')
+import CSVUtil as CSVUtil
+
+testset = CSVUtil.ReadCSV('/home/daizhen/projects/ImagesCategory/data/test_data.csv')
 def default(request):
-    return render_to_response('default.html')
+    index =  request.GET.get('index')
+    index = int(index)
+    #return HttpResponse(index)
+    return render_to_response('default.html',{'index':index, 'filename':testset[index][0]})
     #return HttpResponse('<h1>main pange</h1>')
