3bfa8ee305c62eb0ed61c770fe773b1567012846
Dai Zhen <daizhenbest@gmail.com>
2016-05-24 13:00:12 +0930

    Run test result

--- src/demo/PredictDemoData.py
+++ src/demo/PredictDemoData.py
@@ -70,6 +70,33 @@ def Process_3():
         result_list.append(item)
     model.Release()
     CSVUtil.WriteCSV('demo_data.csv',result_list)        
+
+def ProcessResult():
+     data = CSVUtil.ReadCSV('demo_data.csv')
+     category_acc =0.0
+     subcategory_acc =0.0
+     producttype_acc =0.0
+     productt_acc =0.0
+     for item in data:
+        if item[1] == item[-4]:
+            category_acc+=1
+            
+        if item[2] == item[-3]:
+            subcategory_acc+=1
+            
+        if item[3] == item[-2]:
+            producttype_acc+=1
+            
+        if item[4] == item[-1]:
+            productt_acc+=1
+     category_acc = category_acc*100.0/len(data)
+     subcategory_acc = subcategory_acc*100.0/len(data)
+     producttype_acc = producttype_acc*100.0/len(data)
+     productt_acc = productt_acc*100.0/len(data)       
+     print category_acc,subcategory_acc,producttype_acc,productt_acc
+     '''
+     79.71 90.54 75.41 53.49
+     '''
 if __name__ == "__main__":
-    Process_3()
+    ProcessResult()
     
